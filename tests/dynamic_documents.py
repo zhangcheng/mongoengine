8a18d57aa997df5e7b62ddb92a1e1fac04b344c6
Ross Lawley <ross.lawley@gmail.com>
2011-05-19 11:42:03 +0100

    Updated test name and changed BaseField calls to use db_field
    BaseField name kwarg is being depreciated, in favour of db_field.
    Cleaned up the tests to make independent.
    
    Refs #112

--- tests/dynamic_documents.py
+++ tests/dynamic_documents.py
@@ -5,27 +5,36 @@ from mongoengine.connection import _get_db
 
 test_field = 'test_field'
 test_value = 'test_value'
-    
+
 class FirstDynamicDocumentTest(unittest.TestCase):
 
     def setUp(self):
         connect(db='mongoenginetest')
         self.db = _get_db()
+
         class Person(Document):
             name = StringField()
             age = IntField()
+
         self.Person = Person
+
+    def tearDown(self):
+        self.Person.drop_collection()
 
     def test_create_dynamic_document(self):
         """Ensure that dynamic documents are successfully stored in database.
         """
         test_person = self.Person(name='Test')
+
+        # Create a dynamic_field
         test_person.create_dynamic_field(test_field, test_value)
+
         self.assertTrue(test_person._dynamic_fields.has_key(test_field))
         self.assertTrue(test_person._data.has_key(test_field))
         self.assertTrue(test_person._dynamic_fields.has_key('_dynamic_fields_list'))
         self.assertTrue(test_person._data.has_key('_dynamic_fields_list'))
         test_person.save()
+
         test_person.reload()
         self.assertTrue(test_person._fields.has_key(test_field))
         self.assertTrue(test_person._data.has_key(test_field))
@@ -40,20 +49,31 @@ class SecondDynamicDocumentTest(unittest.TestCase):
     def setUp(self):
         connect(db='mongoenginetest')
         self.db = _get_db()
+
         class Person(Document):
             name = StringField()
             age = IntField()
+
         self.Person = Person
+
+        test_person = self.Person(name='Test')
+        test_person.create_dynamic_field(test_field, test_value)
+        test_person.save()
+
+    def tearDown(self):
+        self.Person.drop_collection()
 
     def test_fetch_dynamic_document(self):
         """Ensure that dynamic documents are successfully read from database.
         """
         collection = self.db[self.Person._meta['collection']]
         test_database_object = collection.find_one({'name': 'Test'})
+
         self.assertTrue(test_database_object.has_key(test_field))
         self.assertEqual(test_database_object[test_field], test_value)
         self.assertTrue(test_database_object.has_key('_dynamic_fields_list'))
         self.assertEqual(test_database_object['_dynamic_fields_list'], ['_dynamic_fields_list', 'test_field'])
+
         dynamic_fields_list = test_database_object['_dynamic_fields_list']
         self.assertTrue(test_field in dynamic_fields_list)
         self.assertTrue('_dynamic_fields_list' in dynamic_fields_list)
@@ -66,8 +86,6 @@ class SecondDynamicDocumentTest(unittest.TestCase):
         self.assertEqual(test_person.test_field, test_value)
         self.assertEqual(test_person._dynamic_fields_list, ['_dynamic_fields_list', 'test_field'])
 
-    def tearDown(self):
-        self.Person.drop_collection()
 
 if __name__ == '__main__':
     unittest.main()
